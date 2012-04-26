0b74555af0d4f596b311682ab0589204fa1ee096
Igor Soares <igorsrs@github.com>
2012-04-26 13:06:35 +0000

    Leves refatoracoes e comentarios na BeanFactory

--- gerador/src/main/java/br/com/cds/mobile/geradores/CodeModelBeanFactory.java
+++ gerador/src/main/java/br/com/cds/mobile/geradores/CodeModelBeanFactory.java
@@ -27,6 +27,7 @@ import com.sun.codemodel.JVar;
 
 public class CodeModelBeanFactory {
 
+	private static final String ERROR_CLONE_NOT_DEEP_COPY_WARNING = "Metodo clone parece nao fazer deep copy de %s da classe %s";
 	private static final String ERRO_MSG_ARGUMENTO_NULL = "argumento null passado para o metodo %s::%s";
 	private JCodeModel jcm;
 
@@ -34,16 +35,11 @@ public class CodeModelBeanFactory {
 		this.jcm = jcm;
 	}
 
-	public void gerarGetterAndSetter(JDefinedClass klass, String nomeCampo) {
-		gerarGetter(klass, nomeCampo);
-		gerarSetter(klass, nomeCampo);
-	}
-
 	public void gerarSetter(JDefinedClass klass, String nomeCampo) {
 		JFieldVar campo = klass.fields().get(nomeCampo);
 		// nomeDoCampo -> NomeDoCampo
 		String nomeCampoCaptalizado = ""+Character.toUpperCase(campo.name().charAt(0))+campo.name().substring(1);
-		/**************************************
+		/* ************************************
 		 *             SETTER                 *
 		 *                                    *
 		 * public void setCampo(Campo campo){ *
@@ -61,7 +57,7 @@ public class CodeModelBeanFactory {
 		JFieldVar campo = klass.fields().get(nomeCampo);
 		// nomeDoCampo -> NomeDoCampo
 		String nomeCampoCaptalizado = ""+Character.toUpperCase(campo.name().charAt(0))+campo.name().substring(1);
-		/*****************************
+		/* ***************************
 		 *        GETTER             *
 		 *                           *
 		 * public Campo getCampo(){  *
@@ -80,6 +76,14 @@ public class CodeModelBeanFactory {
 		corpo._return(campo);
 	}
 
+	/**
+	 * <p>Sobrescreve os metodos hashCode e equals.</p>
+	 * <p>Sempre que dois objetos forem iguais (objA.equals(objB)==true),
+	 *    seus hascodes obriatoriamente sao iguais</p>
+	 * <p>Deve-se notar que o metodo equals eh sempre mais restritivo</p>
+	 * @param klass
+	 * @param javaBeanSchema
+	 */
 	public void gerarHashCodeAndEquals(JDefinedClass klass,JavaBeanSchema javaBeanSchema){
 		Collection<JFieldVar> camposUsados = new ArrayList<JFieldVar>();
 
@@ -94,52 +98,163 @@ public class CodeModelBeanFactory {
 				camposUsados.add(campo);
 		}
 
-		JMethod hashcode = klass.method(JMod.PUBLIC, jcm.INT, "hashCode");
+		gerarMetodoHashCode(klass, camposUsados);
+		gerarMetodoEquals(klass, camposUsados);
+	}
+
+	/**
+	 * Gera metodo hashcode
+	 * @param klass
+	 * @param camposUsados
+	 */
+	private void gerarMetodoHashCode(JDefinedClass klass,
+			Collection<JFieldVar> camposUsados) {
+		String nome = "hashCode";
+		int valInicial = 1;
+		/* ****************************************
+		 *                hashCode                *
+		 *                                        *
+		 * Override                               *
+		 * public int hashCode(){                 *
+		 *     int value = 1;                     *
+		 *     value += ( campoBoolean ? 1 : 0 ); *
+		 *     value += (int) campoNumeric;       *
+		 *     value *= campoObject.hashCode();   *
+		 *     return value;                      *
+		 * }                                      *
+		 *****************************************/
+		JMethod hashcode = klass.method(JMod.PUBLIC, jcm.INT, nome);
 		hashcode.annotate(Override.class);
 		JBlock hashcodeBody = hashcode.body();
-		JVar hashAccumulator = hashcodeBody.decl(jcm.INT, "value",JExpr.lit(1));
+		JVar hashAccumulator = hashcodeBody.decl(jcm.INT, "value",JExpr.lit(valInicial));
 
+		/* ************************************
+		 *            Campo booleano          *
+		 *                                    *
+		 * value += ( campoBoolean ? 1 : 0 ); *
+		 *************************************/
 		for(JFieldVar campoUsado : camposUsados){
 			if(campoUsado.type().equals(jcm.BOOLEAN)){
 				hashcodeBody.assign(
 						hashAccumulator,
-						hashAccumulator.plus(JExpr.direct(campoUsado.name()+"?1:0"))
+						hashAccumulator.plus(JOp.cond(
+								campoUsado,
+								JExpr.lit(1),
+								JExpr.lit(0)
+						))
 				);
 			}
+
+			/* ************************************
+			 *  Outros primitivos sao numericos   *
+			 *                                    *
+			 * value += (int) campoBoolean;       *
+			 *************************************/
 			else if(campoUsado.type().isPrimitive()){
 				hashcodeBody.assign(
 						hashAccumulator,
 						hashAccumulator.plus(JExpr.cast(jcm.INT, campoUsado))
 				);
-			} else {
+			}
+
+			/* ************************************
+			 *               objects              *
+			 *                                    *
+			 * value += campoObject.hashCode();   *
+			 *************************************/
+			else {
 				hashcodeBody.assign(
 						hashAccumulator,
-						hashAccumulator.mul(campoUsado.invoke("hashCode"))
+						hashAccumulator.mul(campoUsado.invoke(nome))
 				);
 			}
 		}
+
+		// retorna o valor no "acumulador"
 		hashcodeBody._return(hashAccumulator);
+	}
 
+	/**
+	 * Sobrescreve metodo equals
+	 * @param klass
+	 * @param camposUsados
+	 */
+	private void gerarMetodoEquals(JDefinedClass klass,
+			Collection<JFieldVar> camposUsados) {
 
+		/* ***************************************************
+		 *                     equals                        *
+		 *                                                   *
+		 * Overide                                           *
+		 * public boolean equals(Object obj){                *
+		 *    if (obj == null) {                             *
+		 *         return false;                             *
+		 *    }                                              *
+		 *    if (!(obj instanceof klass)) {                 *
+		 *        return false;                              *
+		 *    }                                              *
+		 *    Klass other = ((Klass) obj);                   *
+		 *    if (other.campoPrimitivo!= campoPrimitivo) {   *
+		 *        return false;                              *
+		 *    }                                              *
+		 *    if (                                           *
+		 *        (campoObjeto == null) ?                    *
+		 *            other.campoObjeto!= null :             *
+		 *            !campoObjeto.equals(other.campoObjeto) *
+		 *     ) {                                           *
+		 *        return false;                              *
+		 *    }                                              *
+		 *    return true;                                   *
+		 * }                                                 *
+		 ****************************************************/
 		JMethod equals = klass.method(JMod.PUBLIC, jcm.BOOLEAN, "equals");
 		JVar other = equals.param(Object.class, "obj");
 		equals.annotate(Override.class);
 		JBlock equalsBody = equals.body();
 
+		/* ************************
+		 *    if (obj == null) {  *
+		 *         return false;  *
+		 *    }                   *
+		 *************************/
 		equalsBody._if(
 				other.eq(JExpr._null())
 		)._then()._return(JExpr.lit(false));
+
+		/* ***********************************
+		 *    if (!(obj instanceof klass)) { *
+		 *        return false;              *
+		 *    }                              *
+		 *    Klass other = ((Klass) obj);   *
+		 ************************************/
 		equalsBody._if(
 				other._instanceof(klass).not()
 		)._then()._return(JExpr.lit(false));
 		other = equalsBody.decl(klass, "other",JExpr.cast(klass, other));
 
+		
 		for(JFieldVar campoUsado : camposUsados){
+			/* ***************************************************
+			 *    if (other.campoPrimitivo!= campoPrimitivo) {   *
+			 *        return false;                              *
+			 *    }                                              *
+			 ****************************************************/
 			if(campoUsado.type().isPrimitive()){
 				equalsBody._if(
 						other.ref(campoUsado).ne(campoUsado)
 				)._then()._return(JExpr.lit(false));
-			} else {
+			}
+
+			/* **********************************************************
+			 *    if (                                                  *
+			 *        (campoObjeto == null) ?                           *
+			 *            other.campoObjeto!= null :                    *
+			 *            !campoObjeto.equals(other.campoObjeto)        *
+			 *     ) {                                                  *
+			 *        return false;                                     *
+			 *    }                                                     *
+			 ***********************************************************/
+			else {
 				equalsBody._if(
 						JOp.cond(
 								campoUsado.eq(JExpr._null()),
@@ -149,16 +264,28 @@ public class CodeModelBeanFactory {
 				)._then()._return(JExpr.lit(false));
 			}
 		}
+
+		// se nada retornou false, retorna true
 		equalsBody._return(JExpr.lit(true));
 	}
 
+	/**
+	 * gera serialVersionUID para a classe, baseado nos campos
+	 * que nao sao transient, static ou final
+	 * @param klass
+	 */
 	public void gerarSerialVersionUID(JDefinedClass klass){
 		Map<String, JFieldVar> fields = klass.fields();
 		long result = 1;
 		for(String key : fields.keySet()){
 			JFieldVar field = fields.get(key);
-			if( (field.mods().getValue()&JMod.TRANSIENT)!=0)
-				result *= field.name().hashCode() + field.type().fullName().hashCode();
+			if(
+					((field.mods().getValue()&JMod.TRANSIENT)!=0) &&
+					((field.mods().getValue()&JMod.STATIC)!=0) &&
+					((field.mods().getValue()&JMod.FINAL)!=0)
+			){
+				result = result*field.name().hashCode() + field.type().fullName().hashCode();
+			}
 		}
 		klass.field(
 				JMod.PRIVATE|JMod.STATIC|JMod.FINAL,
@@ -168,6 +295,12 @@ public class CodeModelBeanFactory {
 		);
 	}
 
+	/**
+	 * Gera propriedades, campos com getter e/ou setter
+	 * @param classeBean
+	 * @param propriedades
+	 * @return classeBean com propriedades econtradas
+	 */
 	public JDefinedClass gerarPropriedades(JDefinedClass classeBean, Collection<Propriedade> propriedades){
 		for(Propriedade propriedade: propriedades){
 			gerarPropriedade(classeBean, propriedade);
@@ -175,6 +308,11 @@ public class CodeModelBeanFactory {
 		return classeBean;
 	}
 
+	/**
+	 * Gera uma propriedade, campo com getter e/ou setter
+	 * @param classeBean
+	 * @param propriedade
+	 */
 	public void gerarPropriedade(JDefinedClass classeBean, Propriedade propriedade) {
 		JType tipo = getTipo(propriedade.getType());
 		// campos privados
@@ -187,8 +325,21 @@ public class CodeModelBeanFactory {
 			gerarGetter(classeBean, propriedade.getNome());
 	}
 
+	/**
+	 * Gera uma JDefinedClass a partir de seu nome completo
+	 * @param fullyqualifiedName
+	 * @return classe gerada
+	 * @throws JClassAlreadyExistsException
+	 */
 	public JDefinedClass gerarClasse(String fullyqualifiedName)
 			throws JClassAlreadyExistsException {
+		/* *********************************************************
+		 * package nome.completo.do.pacote;                        *
+		 *                                                         *
+		 * public class Klass implements Serializable, Cloneable { *
+		 *                                                         *
+		 * }                                                       *
+		 **********************************************************/
 		JDefinedClass classeBean = jcm._class(
 				JMod.PUBLIC,
 				fullyqualifiedName,
@@ -199,6 +350,11 @@ public class CodeModelBeanFactory {
 		return classeBean;
 	}
 
+	/**
+	 * Gera as constantes com nomes das colunas e da tabela
+	 * @param klass
+	 * @param javaBeanSchema
+	 */
 	public void gerarConstantes(JDefinedClass klass, JavaBeanSchema javaBeanSchema) {
 		klass.field(
 				JMod.PUBLIC|JMod.STATIC|JMod.FINAL,
@@ -218,10 +374,36 @@ public class CodeModelBeanFactory {
 		}
 	}
 
-
+	/**
+	 * Sobrescreve metodo clone, da interface Cloneable
+	 * @param klass
+	 * @param javaBeanSchema
+	 */
 	public void gerarMetodoClone(JDefinedClass klass, JavaBeanSchema javaBeanSchema){
+		/* **********************************************
+		 * public Klass clone() {                       *
+		 *     klass obj;                               *
+		 *     try {                                    *
+		 *         obj = ((Klass) super.clone());       *
+		 *     } catch (CloneNotSupportedException e) { *
+		 *         return null;                         *
+		 *     }                                        *
+		 *     obj.data = new Date(data);               *
+		 *     obj.campoAssociado = null;               *
+		 *     return obj;                              *
+		 * }                                            *
+		 ***********************************************/
 		JMethod clone = klass.method(JMod.PUBLIC, klass, "clone");
 		JBlock corpo = clone.body();
+
+		/* **********************************************
+		 *     klass obj;                               *
+		 *     try {                                    *
+		 *         obj = ((Klass) super.clone());       *
+		 *     } catch (CloneNotSupportedException e) { *
+		 *         return null;                         *
+		 *     }                                        *
+		 ***********************************************/
 		JVar newClone = corpo.decl(klass, "obj");
 		JTryBlock tryCast = corpo._try();
 		tryCast.body().assign(
@@ -232,9 +414,11 @@ public class CodeModelBeanFactory {
 			._catch(jcm.ref(CloneNotSupportedException.class))
 			.body()
 			._return(JExpr._null());
+
 		for(String coluna : ColunasUtils.colunasOrdenadasDoJavaBeanSchema(javaBeanSchema)){
 			Propriedade propriedade = javaBeanSchema.getPropriedade(coluna);
 			JFieldVar campo = klass.fields().get(propriedade.getNome());
+
 			// se for necessario "zerar"o id
 			//if(propriedade.equals(javaBeanSchema.getPrimaryKey()))
 			//	corpo.assign(
@@ -242,11 +426,15 @@ public class CodeModelBeanFactory {
 			//			JExpr.direct(CodeModelDaoFactory.ID_PADRAO)
 			//	);
 
-			// para usar o clone binario da classe object, nada eh feito
+			// usar o valor obtido com o clone binario da interface cloneable
 			if(campo.type().isPrimitive()||campo.type().equals(getTipo(String.class))) {
 				continue;
 			}
 
+			/* **********************************************
+			 * //reescreve a datas, criando novos objetos   *
+			 *     obj.data = new Date(data);               *
+			 ***********************************************/
 			else if(propriedade.getType().equals(Date.class)){
 				corpo.assign(
 						newClone.ref(campo),
@@ -254,9 +442,21 @@ public class CodeModelBeanFactory {
 				);
 			}
 
-			else
-				corpo.assign(newClone.ref(campo), campo);
+			// Se chegar aqui, deu erro
+			else{
+				System.err.printf(
+						ERROR_CLONE_NOT_DEEP_COPY_WARNING,
+						campo.name(),
+						klass.name()
+				);
+			};
 		}
+
+		/* **********************************************
+		 * //estes campos devem ser buscados novamente  *
+		 * // no banco                                  *
+		 *     obj.campoAssociado = null;               *
+		 ***********************************************/
 		for(String key : klass.fields().keySet()){
 			JFieldVar campo = klass.fields().get(key);
 			if( (campo.mods().getValue()&JMod.TRANSIENT)!=0 ){
@@ -264,36 +464,16 @@ public class CodeModelBeanFactory {
 			}
 		}
 
+		// retorna o novo clone
 		corpo._return(newClone);
 	}
 
-//	public void gerarAssociacaoToOne(JDefinedClass klass, JDefinedClass estrangeira, String idEstrangeira){
-//		// private IdClass idEstrangeira;
-//		// JFieldVar idCampo = klass.fields().get(idEstrangeira);
-//		// private ClasseEstrangeira estrangeira;
-//		JFieldVar campo = klass.field(
-//				JMod.PRIVATE,
-//				estrangeira,
-//				estrangeira.name(),
-//				JExpr._null()
-//		);
-//		// getters e setters
-//		gerarGetterAndSetter(klass, campo.name());
-//	}
-//
-//	public void gerarAssociacaoToMany(JDefinedClass klass, JDefinedClass estrangeira, String idEstrangeira){
-//		// private ClasseEstrangeira estrangeira;
-//		JFieldVar campo = klass.field(
-//				JMod.PRIVATE,
-//				jcm.ref(Collection.class).narrow(estrangeira),
-//				// TODO pluralizar
-//				estrangeira.name(),
-//				JExpr._null()
-//		);
-//		// getters e setters
-//		gerarGetterAndSetter(klass, campo.name());
-//	}
 
+	/**
+	 * Retorna o tipo do CodeModel para a classe
+	 * @param klass
+	 * @return
+	 */
 	public JType getTipo(Class<?> klass){
 		if(klass==null)
 			throw new RuntimeException(String.format(
