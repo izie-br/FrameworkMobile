cf0e5ab11225ef0b1b093f6f230a467f25ced6b0
Igor Soares <igorsrs@github.com>
2012-06-05 20:44:39 +0000

    Reformulacao da classe "Q"
    - reformulacao classe Q
    - novas classes Table e Column (inner);
    - uso de generics para tipagem estatica dos argumentos das colunas.

---
+++ lib/src/main/java/br/com/cds/mobile/framework/query/Table.java
@@ -0,0 +1,72 @@
+package br.com.cds.mobile.framework.query;
+
+public class Table {
+
+    private String name;
+
+    public Table (String name){
+        this.name  = name;
+    }
+
+    public <T> Table.Column<T> addColumn (String name){
+        Table.Column<T> col = new Table.Column<T>();
+        col.name = name;
+        return col;
+    }
+
+    /**
+     * Gets the name for this instance.
+     *
+     * @return name.
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    public class Column<T> {
+        private String name;
+
+        /**
+         * Gets the name for this instance.
+         *
+         * @return name.
+         */
+        public String getName() {
+            return this.name;
+        }
+
+        public Table getTable(){
+            return Table.this;
+        }
+
+        public Q eq(T arg){
+            return new Q(this, Q.Op1x1.EQ, arg);
+        }
+
+        public Q ne(T arg){
+            return new Q(this, Q.Op1x1.NE, arg);
+        }
+
+        public Q lt(T arg){
+            return new Q(this, Q.Op1x1.LT, arg);
+        }
+
+        public Q le(T arg){
+            return new Q(this, Q.Op1x1.LE, arg);
+        }
+
+        public Q gt(T arg){
+            return new Q(this, Q.Op1x1.GT, arg);
+        }
+
+        public Q ge(T arg){
+            return new Q(this, Q.Op1x1.GE, arg);
+        }
+
+        public Q in(T arg){
+            return new Q(this, Q.Op1x1.IN, arg);
+        }
+
+    }
+
+}
