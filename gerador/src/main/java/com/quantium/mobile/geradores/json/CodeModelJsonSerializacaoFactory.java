38d94610cdd1e04428dd35b8015beed22dd1e75a
Igor Soares <igorsrs@github.com>
2012-07-02 21:24:49 +0000

    Refatoracao do json factory

--- gerador/src/main/java/com/quantium/mobile/geradores/json/CodeModelJsonSerializacaoFactory.java
+++ gerador/src/main/java/com/quantium/mobile/geradores/json/CodeModelJsonSerializacaoFactory.java
@@ -8,8 +8,6 @@ import org.json.JSONObject;
 
 import com.quantium.mobile.framework.utils.DateUtil;
 import com.quantium.mobile.geradores.dao.CodeModelDaoFactory;
-import com.quantium.mobile.geradores.filters.associacao.Associacao;
-import com.quantium.mobile.geradores.filters.associacao.AssociacaoOneToMany;
 import com.quantium.mobile.geradores.javabean.JavaBeanSchema;
 import com.quantium.mobile.geradores.javabean.Propriedade;
 import com.quantium.mobile.geradores.util.ColumnsUtils;
@@ -35,32 +33,76 @@ public class CodeModelJsonSerializacaoFactory {
 		this.jcm = jcm;
 	}
 
+	/**
+	 * Gera os metodos de serialização de de-serialização de json
+	 * toJson() e jsonToObjectWithPrototype(JSONObject).
+	 * 
+	 * @param klass
+	 * @param javaBeanSchema
+	 */
 	public void gerarMetodosDeSerializacaoJson(JDefinedClass klass,JavaBeanSchema javaBeanSchema){
-
 		String primaryKeyNome = javaBeanSchema.getPrimaryKey()==null ?
+				//
 				null :
+				//
 				javaBeanSchema.getPrimaryKey().getNome();
+		generateToJsonMethod(klass, javaBeanSchema, primaryKeyNome);
+		generateJsonToObjectMethod(klass, javaBeanSchema, primaryKeyNome);
+	}
 
+
+	private void generateToJsonMethod(JDefinedClass klass,
+			JavaBeanSchema javaBeanSchema, String primaryKeyNome) {
+		/*
+		 * public JSONObject toJson() {
+		 *     try {
+		 *         JSONObject jsonObj = new JSONObject();
+		 *         if (id!= 0L) {
+		 *             jsonObj.put("id", id);
+		 *         }
+		 *         jsonObj.put("campo", campo);
+		 *         ...
+		 *         jsonObj.put("data", DateUtil.dateToString(data));
+		 *         ...
+		 *         return jsonObj;
+		 *     } catch (JSONException e) {
+		 *         throw new RuntimeException(e);
+		 *     }
+		 * }
+		 */
 		JMethod toJson = klass.method(JMod.PUBLIC, jcm.ref(JSONObject.class), "toJson");
-		//toJson._throws(JSONException.class);
-		JBlock corpoMetodo = toJson.body();
-		JTryBlock tryBlock = corpoMetodo._try();
+		JTryBlock tryBlock = toJson.body()._try();
 		JBlock corpo = tryBlock.body();
+
+		// JSONObject jsonObj = new JSONObject();
 		JVar jsonObj = corpo.decl(jcm.ref(JSONObject.class), "jsonObj", JExpr._new(jcm.ref(JSONObject.class)));
+
 		for(String coluna : ColumnsUtils.orderedColumnsFromJavaBeanSchema(javaBeanSchema)){
 			Propriedade propriedade = javaBeanSchema.getPropriedade(coluna);
 			JFieldVar campo = klass.fields().get(propriedade.getNome());
+
+			// jsonObj.put("campo", ...
 			JInvocation setJsonField = jsonObj.invoke("put").arg(JExpr.lit(coluna));
 			if(propriedade.getNome().equals(primaryKeyNome)){
+				/*
+				 * if (id!= 0L) {
+				 *     jsonObj.put("id", id);
+				 * }
+				 */
 				corpo._if(campo.ne(JExpr.lit(CodeModelDaoFactory.ID_PADRAO)))
 					._then().add(setJsonField.arg(campo));
 			}
 			else {
-			JExpression value = campo;
-			if(propriedade.getType().equals(Date.class)){
-				value = jcm.ref(DateUtil.class).staticInvoke("dateToString").arg(value);
-			}
-			corpo.add(setJsonField.arg(value));
+				JExpression value = campo;
+				if(propriedade.getType().equals(Date.class)){
+					value = jcm.ref(DateUtil.class).staticInvoke("dateToString").arg(value);
+				}
+				/*
+				 * jsonObj.put("campo", campo);
+				 *  // ou se for Date
+				 * jsonObj.put("data", DateUtil.dateToString(data));
+				 */
+				corpo.add(setJsonField.arg(value));
 			}
 		}
 		corpo._return(jsonObj);
@@ -76,43 +118,53 @@ public class CodeModelJsonSerializacaoFactory {
 		JVar except = catchBlock.param("e");
 		catchBlock.body()
 			._throw(JExpr._new(jcm.ref(RuntimeException.class)).arg(except));
+	}
 
+	private void generateJsonToObjectMethod(JDefinedClass klass,
+			JavaBeanSchema javaBeanSchema, String primaryKeyNome) {
 		/*
-		 * public static jsonToObject(JsonObject json){
-		 * 
+		 * public Customer jsonToObjectWithPrototype(JSONObject json)
+		 *     throws JSONException
+		 * {
+		 *     Customer obj = clone();
+		 *     obj.id = json.optLong("id", 0L);
+		 *     obj.campo = json.optString("campo",campo);
+		 *     obj.data = DateUtil.stringToDate(
+		 *         json.optString("data", DateUtil.dateToString(data))
+		 *     );
+		 *     return obj;
+		 * }
 		 */
 		JMethod jsonToObjectWithPrototype = klass.method(JMod.PUBLIC, klass, "jsonToObjectWithPrototype");
 		jsonToObjectWithPrototype._throws(JSONException.class);
-		jsonObj = jsonToObjectWithPrototype.param(JSONObject.class, "json");
-		corpo = jsonToObjectWithPrototype.body();
+		JVar jsonObj = jsonToObjectWithPrototype.param(JSONObject.class, "json");
+		JBlock corpo = jsonToObjectWithPrototype.body();
+
+		// Customer obj = clone();
 		JVar obj = corpo.decl(klass, "obj", JExpr.invoke("clone"));
+
 		for(String coluna : ColumnsUtils.orderedColumnsFromJavaBeanSchema(javaBeanSchema)){
 			Propriedade propriedade = javaBeanSchema.getPropriedade(coluna);
-			JInvocation value;
-			value = jsonObj.invoke(SQLiteGeradorUtils.metodoGetDoJsonParaClasse(propriedade.getType()))
-				.arg(coluna);
+			// obj.campo = json.optString("campo",campo);
+			JInvocation value = jsonObj.invoke(
+					SQLiteGeradorUtils.metodoOptDoJsonParaClasse(
+						propriedade.getType()
+					)
+				).arg(coluna)
+				.arg(klass.fields().get(propriedade.getNome()));
 			if(propriedade.getNome().equals(primaryKeyNome)){
+				// obj.id = json.optLong("id", 0L);
 				value = jsonObj.invoke(SQLiteGeradorUtils.metodoOptDoJsonParaClasse(propriedade.getType()))
 					.arg(coluna)
 					.arg(JExpr.lit(CodeModelDaoFactory.ID_PADRAO));
-
-			}
-			else{
-				value = jsonObj.invoke(SQLiteGeradorUtils.metodoGetDoJsonParaClasse(propriedade.getType()))
-						.arg(coluna);
-			}
-			for(Associacao associacao : javaBeanSchema.getAssociacoes()){
-				if(associacao instanceof AssociacaoOneToMany){
-					AssociacaoOneToMany associacaoOneToMany = (AssociacaoOneToMany)associacao;
-					if(associacaoOneToMany.getKeyToA().equals(coluna)){
-						value = jsonObj.invoke(SQLiteGeradorUtils.metodoOptDoJsonParaClasse(propriedade.getType()))
-								.arg(coluna)
-								.arg(JExpr.lit(CodeModelDaoFactory.ID_PADRAO));
-					}
-				}
-			}
-			if(propriedade.getType().equals(Date.class)){
-				value = jcm.ref(DateUtil.class).staticInvoke("stringToDate").arg(value);
+			} else if(propriedade.getType().equals(Date.class)){
+				/*
+				 *     obj.data = DateUtil.stringToDate(
+				 *         json.optString("data", DateUtil.dateToString(data))
+				 *     );
+				 */
+				value = jcm.ref(DateUtil.class).staticInvoke("stringToDate")
+					.arg(value);
 			}
 			corpo.assign(
 					obj.ref(klass.fields().get(propriedade.getNome())),
@@ -120,8 +172,6 @@ public class CodeModelJsonSerializacaoFactory {
 			);
 		}
 		corpo._return(obj);
-		
-		//Jmethod
 	}
 
 }
