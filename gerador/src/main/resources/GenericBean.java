779a7729777ac0cad01b31949402300d1b4ae72a
Igor Soares <igorsrs@github.com>
2012-08-29 19:26:51 +0000

    [Bugfix]
    + Alterei o resource de GenericBean.java, para sincroniza-lo com o app teste.

--- gerador/src/main/resources/GenericBean.java
+++ gerador/src/main/resources/GenericBean.java
@@ -1,8 +1,9 @@
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.database.SQLException;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.quantium.mobile.framework.Save;
 
@@ -10,9 +11,13 @@ import com.quantium.mobile.framework.Save;
 public abstract class GenericBean
     implements Serializable, Cloneable
 {
-    public abstract JSONObject toJson();
-    public abstract GenericBean jsonToObjectWithPrototype(JSONObject json)
-        throws JSONException;
+    public abstract Map<String,Object> toMap(Map<String,Object> map);
+
+    public Map<String,Object> toMap(){
+        return toMap(new HashMap<String, Object>());
+    }
+
+    public abstract GenericBean mapToObject(Map<String,Object> json);
     public abstract boolean save(int flags) throws SQLException;
     public abstract boolean delete();
 
