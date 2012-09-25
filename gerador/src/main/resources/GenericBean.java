025bd3e5e247cbbc9a6eebad88c5e57c79d61999
Igor Soares <igorsrs@github.com>
2012-09-25 12:42:31 +0000

    [BugFix]
    + Atualizacao do template do GenericBean

--- gerador/src/main/resources/GenericBean.java
+++ gerador/src/main/resources/GenericBean.java
@@ -3,11 +3,6 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-import android.database.SQLException;
-
-import com.quantium.mobile.framework.Save;
-import com.quantium.mobile.framework.Session;
-
 @SuppressWarnings("serial")
 public abstract class GenericBean
     implements Serializable, Cloneable
@@ -19,15 +14,5 @@ public abstract class GenericBean
     }
 
     public abstract GenericBean mapToObject(Map<String,Object> json);
-    public abstract boolean save(Session session, int flags) throws SQLException;
-    public abstract boolean delete();
-
-    public boolean save(Session session) throws SQLException{
-        return save(session, Save.INSERT_IF_NULL_PK);
-    }
-
-    protected int onPreSave(int flags){
-        return flags;
-    }
 
 }
