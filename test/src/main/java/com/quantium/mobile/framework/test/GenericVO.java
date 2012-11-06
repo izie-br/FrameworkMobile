1423367226fb6926304758ee14d150843a96b88b
Igor Soares <igorsrs@github.com>
2012-11-06 16:19:31 +0000

    [BugFix]
    + Adicionei a interface GenericVO que deveria estar no commit anterior;

---
+++ test/src/main/java/com/quantium/mobile/framework/test/GenericVO.java
@@ -0,0 +1,17 @@
+package com.quantium.mobile.framework.test;
+
+import java.util.Map;
+
+public interface GenericVO {
+
+	public abstract void toMap(Map<String, Object> map);
+
+	public abstract Map<String, Object> toMap();
+
+	public abstract void triggerObserver(String column);
+
+	public abstract int hashCodeImpl();
+
+	public abstract boolean equalsImpl(Object obj);
+
+}
