2b0a2c2f2075202da41c6404047008bc78d93b30
Igor Soares <igorsrs@github.com>
2012-11-21 13:54:57 +0000

    [Limpeza]
    + Renomeei QueryTests para SQLiteTest e movi alguns testes, que nao dependiam
      de usar o banco em si, para o lib_android.

--- lib_android/src/test/java/com/quantium/mobile/framework/libandroidtest/query/QueryTest.java
+++ lib_android/src/test/java/com/quantium/mobile/framework/libandroidtest/query/QueryTest.java
@@ -1,35 +1,23 @@
-package com.quantium.mobile.framework.test.query;
+package com.quantium.mobile.framework.libandroidtest.query;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import android.test.ActivityInstrumentationTestCase2;
+import org.junit.Test;
 
-import com.quantium.mobile.framework.DAO;
+import static org.junit.Assert.*;
+
 import com.quantium.mobile.framework.query.Q;
 import com.quantium.mobile.framework.query.QSQLProvider;
 import com.quantium.mobile.framework.query.QueryParseException;
-import com.quantium.mobile.framework.query.QuerySet;
 import com.quantium.mobile.framework.query.Table;
-import com.quantium.mobile.framework.test.SessionFacade;
-import com.quantium.mobile.framework.test.TestActivity;
 import com.quantium.mobile.framework.utils.SQLiteUtils;
-import com.quantium.mobile.framework.utils.StringUtil;
-import com.quantium.mobile.framework.test.gen.Author;
-import com.quantium.mobile.framework.test.gen.AuthorImpl;
 
-public class QueryTests  extends ActivityInstrumentationTestCase2<TestActivity> {
+public class QueryTest {
 
-	SessionFacade facade = new SessionFacade();
-	public QueryTests() {
-		super("com.quantium.mobile.framework.test", TestActivity.class);
-	}
-	
+	@Test
 	public void testQString(){
 		// dados para teste
 		Table t = new Table("tab");
@@ -81,6 +69,7 @@ public class QueryTests  extends ActivityInstrumentationTestCase2<TestActivity> 
 		));
 	}
 
+	@Test
 	public void testJoinQstring () {
 
 		Table table1 = new Table("tab_1");
@@ -141,6 +130,7 @@ public class QueryTests  extends ActivityInstrumentationTestCase2<TestActivity> 
 		assertTrue(select.matches(selectRegex));
 	}
 
+	@Test
 	public void testNullArg () {
 		Table t = new Table("tab");
 		Table.Column<Integer> colInt = t.addColumn(Integer.class, "col_int");
@@ -173,45 +163,7 @@ public class QueryTests  extends ActivityInstrumentationTestCase2<TestActivity> 
 		}
 	}
 
-	public void testLikeAndGlob(){
-		DAO<Author> dao = facade.getDAOFactory().getDaoFor(Author.class);
-		Author author1 = new AuthorImpl();
-		author1.setName("um nome");
-		Date now = new Date();
-		now = new Date(now.getYear(), now.getMonth(), now.getDate(),
-				now.getHours(), now.getMinutes());
-		author1.setCreatedAt(now);
-		Author author2 = new AuthorImpl();
-		author2.setName("outro nome");
-		author2.setCreatedAt(now);
-		Author author3 = new AuthorImpl();
-		author3.setName("outro");
-		author3.setCreatedAt(now);
-		try {
-			assertTrue(dao.save(author1));
-			assertTrue(dao.save(author2));
-			assertTrue(dao.save(author3));
-		} catch (IOException e) {
-			fail(StringUtil.getStackTrace(e));
-		}
-		// buscas com LIKE
-		Collection<Author> authors = dao.query(
-				Q.like(Author.NAME,"%no_e"))
-			.all();
-		assertNotNull(authors);
-		assertEquals(2, authors.size());
-		assertTrue(authors.contains(author1));
-		assertTrue(authors.contains(author2));
-		// buscas com GLOB
-		authors = dao.query(
-				Q.glob(Author.NAME,"*[nm]?[mw]e"))
-			.all();
-		assertNotNull(authors);
-		assertEquals(2, authors.size());
-		assertTrue(authors.contains(author1));
-		assertTrue(authors.contains(author2));
-	}
-
+	@Test
 	public void testInOperator(){
 		Table table1 = new Table("tab_1");
 		Table.Column<Long> colTab1Id = table1.addColumn(Long.class, "id");
@@ -237,6 +189,7 @@ public class QueryTests  extends ActivityInstrumentationTestCase2<TestActivity> 
 
 	}
 
+	@Test
 	public void testImmutable(){
 		Table table1 = new Table("tab_1");
 		Table.Column<Long> colTab1Id = table1.addColumn(Long.class, "id");
@@ -279,51 +232,6 @@ public class QueryTests  extends ActivityInstrumentationTestCase2<TestActivity> 
 					new Table.Column<?>[]{colTab1Id},
 					new ArrayList<String>());
 			assertFalse( q4Str.equals(q1Str));
-		}
-	}
-
-	public void testImmutableQuerySet() throws Exception{
-		DAO<Author> dao = facade.getDAOFactory().getDaoFor(Author.class);
-		for (int i=0; i< 10; i++){
-			Author author1 = new AuthorImpl();
-			author1.setName("nome["+i+"]");
-			author1.setCreatedAt(new Date());
-			try {
-				dao.save(author1);
-			} catch (IOException e) {
-				fail(StringUtil.getStackTrace(e));
-			}
-		}
-		final QuerySet<Author> qs1 = dao.query();
-		List<Author> list1 = qs1.all();
-		int qty = list1.size();
-		Author first = qs1.first();
-
-		{
-			QuerySet<Author> qs2 = qs1.filter(Author.NAME.eq("nome["+9+"]"));
-			List<Author> list2 = qs2.all();
-			list1 = qs1.all();
-			assertFalse(qs1 == qs2);
-			assertEquals(qty, list1.size());
-			assertTrue(list1.size() > list2.size() );
-		}
-
-		{
-			QuerySet<Author> qs3 = qs1.limit(3);
-			List<Author> list2 = qs3.all();
-			list1 = qs1.all();
-			assertFalse(qs1 == qs3);
-			assertEquals(qty, list1.size());
-			assertTrue( list1.size() > list2.size() );
-		}
-
-		{
-			QuerySet<Author> qs3 = qs1.orderBy(Author.ID, Q.DESC);
-			list1 = qs1.all();
-			assertFalse(qs1 == qs3);
-			List<Author> list3 = qs3.all();
-			assertEquals(first, list1.get(0));
-			assertFalse( first.equals(list3.get(0)) );
 		}
 	}
 
