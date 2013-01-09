3d20181c2da5d88a73c7a50ff52bbd240509f37a
Igor Soares <igorsrs@github.com>
2013-01-09 13:56:41 +0000

    [Limpeza]
    + Getters e Setters movidos para template proprio.

---
+++ gerador/src/main/resources/VO.java.d/gettersAndSetters.java
@@ -0,0 +1,54 @@
+#foreach ($field in $fields)
+#**##if ( ($interface || $implementation) && $field.Get)
+#******#    public ${field.Type} ${getter[$field]} () #if($implementation){
+#******#        return ${field.LowerCamel};
+#******#    }#else;#end
+#******#
+#******#
+#**##end
+#**##if ( (!$associationForField[$field] && $implementation) ||
+          (!$editableInterface && $field.Set && !$primaryKeys.contains($field)) ||
+          ($primaryKeys.contains($field) && $editableInterface && !$associationForField[$field])
+        )
+#******#    public void set${field.UpperCamel}(${field.Type} ${field.LowerCamel}) #if ($implementation){
+#******#        this.${field.LowerCamel} = ${field.LowerCamel};
+#******#        triggerObserver("${field.LowerAndUnderscores}");
+#******#    }#else;#end
+#******#
+#******#
+#**##end
+#end
+##
+#foreach ($association in $manyToOneAssociations)
+#**##if ($interface || $implementation)
+#******#    public ${association.Klass} get${association.Klass}() #if ($implementation){
+#******#        return _${association.Klass};
+#******#    }#else;#end
+#******#
+#******#
+#**##end
+#**##if (!$primaryKeys.contains($association.ForeignKey) || $implementation || $editableInterface )
+#******#    public void set${association.Klass}(${association.Klass} obj) #if ($implementation) {
+#******#        _${association.Klass} = obj;
+#******#    }#else;#end
+#******#
+#******#
+#**##end
+#end
+##
+#foreach ($association in $toManyAssociations)
+#**##if ($interface || $implementation)
+#******#    public QuerySet<${association.Klass}> get${association.Pluralized}() #if ($implementation) {
+#******#        return _${association.Pluralized};
+#******#    }#else;#end
+#******#
+#******#
+#**##end
+#**##if ($implementation || $editableInterface)
+#******#    public void set${association.Pluralized}(QuerySet<${association.Klass}> querySet) #if ($implementation) {
+#******#        this._${association.Pluralized} = querySet;
+#******#    }#else;#end
+#******#
+#******#
+#**##end
+#end
