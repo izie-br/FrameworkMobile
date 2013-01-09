838e7c32e81c2a7aa7aef3f8e39cdef28a4ddbec
Igor Soares <igorsrs@github.com>
2013-01-09 13:40:21 +0000

    [Limpeza]
    + Movi implementacoes metodos do GenericBean para template proprio.

--- gerador/src/main/resources/VO.java
+++ gerador/src/main/resources/VO.java
@@ -32,6 +32,8 @@ package $package;
 ##
 #if ($implementation)
 #**##parse("VO.java.d/fieldsAndConstructor.java")
+#**#
+#**##parse("VO.java.d/interfaceMethodsImpl.java")
 #end
 ##
 #foreach ($field in $fields)
@@ -88,85 +90,5 @@ package $package;
 #******#
 #**##end
 #end
-##
-##
-#if ($implementation)
-#**#    @Override
-#**#    public void toMap(Map<String, Object> map) {
-#**##foreach ($field in $fields)
-#******##if ($field.SerializationAlias)
-#**********##set ($alias = $field.SerializationAlias)
-#******##else
-#**********##set ($alias = $field.LowerAndUnderscores)
-#******##end
-#******###
-#******##set ($association = false)
-#******##if ($associationForField[$field])
-#**********##set ($association = $associationForField[$field])
-#**********#        $field.Type $field.LowerCamel = (_${association.Klass} == null)? 0 : _${association.Klass}.get${association.ReferenceKey.UpperCamel}();
-#******##end
-#******##if ($primaryKeys.contains($field) || $association)
-#**********#        if (${field.LowerCamel} != ${defaultId}) {
-#**********#            map.put("${alias}", ${field.LowerCamel});
-#**********#        }
-#******##else
-#**********#        map.put("${alias}", ${field.LowerCamel});
-#******##end
-#**##end
-#**#    }
-#**#
-#**#    @Override
-#**#    public int hashCodeImpl() {
-#**#        int value = 1;
-#**##foreach ($field in $fields)
-#******##if (!$associationForField[$field])
-#**********##if ($field.Klass.equals("Boolean"))
-#**************#        value += (${field.LowerCamel}) ? 1 : 0;
-#**********##elseif ($field.Klass.equals("Integer") || $field.Klass.equals("Long") || $field.Klass.equals("Double"))
-#**************#        value +=(int) ${field.LowerCamel};
-#**********##else
-#**************#        value *= (${field.LowerCamel} == null) ? 1 : ${field.LowerCamel}.hashCode();
-#**********##end
-#******##end
-#**##end
-#**#        return value;
-#**#    }
-#**#
-#**#    @Override
-#**#    public boolean equalsImpl(Object obj) {
-#**#        if (obj == null) {
-#**#            return false;
-#**#        }
-#**#        if ( !(obj instanceof ${Klass}) ) {
-#**#            return false;
-#**#        }
-#**#        ${Klass} other = ((${Klass}) obj);
-#**##foreach ($field in $fields)
-#******##if ($associationForField[$field])
-#**********##set ($association = $associationForField[$field])
-#**********##set ($otherAssoc = "other${association.Klass}")
-#**********#        ${association.Klass} ${otherAssoc} = other.get${association.Klass}();
-#**********#        if (_${association.Klass} == null){
-#**********#            if (${otherAssoc} != null)
-#**********#                return false;
-#**********#        } else {
-#**********#            if (${otherAssoc} == null)
-#**********#                return false;
-#**********#            if (_${association.Klass}.get${association.ReferenceKey.UpperCamel}() != ${otherAssoc}.get${association.ReferenceKey.UpperCamel}())
-#**********#                return false;
-#**********#        }
-#******##else
-#**********##if ($field.Klass.equals("Boolean") || $field.Klass.equals("Long")|| $field.Klass.equals("Integer") || $field.Klass.equals("Double"))
-#**************#        if(${field.LowerCamel} != other.${getter[$field]}())
-#**************#            return false;
-#**********##else
-#**************#        if( ( ${field.LowerCamel}==null)? (other.${getter[$field]}() != null) :  !${field.LowerCamel}.equals(other.${getter[$field]}()) )
-#**************#            return false;
-#**********##end
-#******##end
-#**##end
-#**#        return true;
-#**#    }
 
-#end##if ($implementation)
 }
