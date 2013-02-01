fea7dfcbecc2e68ba04720295a145effc399ff54
Igor Soares <igorsrs@github.com>
2013-02-01 11:53:45 +0000

    [BugFix]
    + Correcao do bug que filtrava incorretamente um nome de classe igual ao nome
      do modulo;
    + Teste para conferir este bug;

--- gerador/src/main/java/com/quantium/mobile/geradores/filters/ModuleNameOnTablePrefixFilter.java
+++ gerador/src/main/java/com/quantium/mobile/geradores/filters/ModuleNameOnTablePrefixFilter.java
@@ -8,7 +8,6 @@ import com.quantium.mobile.geradores.util.Constants;
 
 public class ModuleNameOnTablePrefixFilter extends TabelaSchemaFilter {
 
-	private static final String SEPARATOR = "_";
 
 	@Override
 	public Table getTable() {
@@ -30,19 +29,26 @@ public class ModuleNameOnTablePrefixFilter extends TabelaSchemaFilter {
 		return builder.get ();
 	}
 
-	@Override
-	public String getName() {
-		String nome = super.getName();
-		String module = super.getModule ();
-		if (module.equals (Constants.DEFAULT_GENERATOR_CONFIG))
-			return nome;
-//		if(nome.startsWith(module))
-//			nome =  nome.substring(module.length());
-		if (nome.startsWith (SEPARATOR))
-			nome = nome.substring (SEPARATOR.length ());
-		return nome;
-	}
-
+/*
+ * Se precisar de modulos com input de SQL:
+ *  - retrabalhar o metodo abaixo, pois do jeito que esta,
+ *    ele buga se o nome do modulo for igual ao, ou apenas contido no inicio,
+ *    nome de uma classe, que fica "" apos passar este filtro;
+ *
+ *	private static final String SEPARATOR = "_";
+ *	@Override
+ *	public String getName() {
+ *		String nome = super.getName();
+ *		String module = super.getModule ();
+ *		if (module.equals (Constants.DEFAULT_GENERATOR_CONFIG))
+ *			return nome;
+ *		if(nome.startsWith(module))
+ *			nome =  nome.substring(module.length());
+ *		if (nome.startsWith (SEPARATOR))
+ *			nome = nome.substring (SEPARATOR.length ());
+ *		return nome;
+ *	}
+ */
 	public static class Factory implements TabelaSchemaFilterFactory {
 
 		@Override
---
+++ test/migrations/db_versao_6.sql
@@ -0,0 +1,1 @@
+ALTER TABLE tb_documents_document RENAME TO tb_document_document;
