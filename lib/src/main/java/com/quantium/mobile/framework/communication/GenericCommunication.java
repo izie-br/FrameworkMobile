e3e54bc6fdcd684b7744c9fd1e89e083eea01b80
Igor Soares <igorsrs@github.com>
2013-02-05 17:11:28 +0000

    [Solicitacao]
    + Movi as classes de JSON para o modulo "lib".

--- lib/src/main/java/com/quantium/mobile/framework/communication/GenericCommunication.java
+++ lib/src/main/java/com/quantium/mobile/framework/communication/GenericCommunication.java
@@ -1,15 +1,8 @@
 package com.quantium.mobile.framework.communication;
 
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URI;
-import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -28,14 +21,13 @@ import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
-import com.quantium.mobile.framework.Tarefa;
 import com.quantium.mobile.framework.logging.LogPadrao;
 
 
 public abstract class GenericCommunication implements Communication {
 
 	protected static final String DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded";
-	private static final int DEFAULT_BUFFER = 1024;
+//	private static final int DEFAULT_BUFFER = 1024;
 	protected static final int CONNECTION_RETRY_COUNT = 5;
 	private static final int SO_TIMEOUT = 90000;
 	private static final int CONNECTION_TIMEOUT = 15000;
@@ -43,8 +35,8 @@ public abstract class GenericCommunication implements Communication {
 
 	public static final byte GET = 0;
 	public static final byte POST = 1;
-	private static final String UNABLE_TO_CREATE_EXISTING_FILE =
-			"UNABLE_TO_CREATE_EXISTING_FILE";
+//	private static final String UNABLE_TO_CREATE_EXISTING_FILE =
+//			"UNABLE_TO_CREATE_EXISTING_FILE";
 
 	private static boolean connected = true;
 	private static ConnectionStatusChangeListener connectionListeners[];
@@ -229,72 +221,72 @@ public abstract class GenericCommunication implements Communication {
 		return new HttpPost();
 	}
 
-	public static boolean downloadFile(
-			String urlString, String path, String arquivo,
-			Tarefa<?, ?> tarefa, String sincronia
-	) throws RuntimeException {
-		try {
-			// LogPadrao.d("Com:" + urlString);
-			HttpURLConnection c;
-			try {
-				URL url = new URL(urlString);
-				c = (HttpURLConnection) url.openConnection();
-				c.setRequestMethod("GET");
-				c.setDoOutput(true);
-			} catch (MalformedURLException e) {
-				LogPadrao.e(e);
-				return false;
-			} catch (ProtocolException e){
-				LogPadrao.e (e);
-				return false;
-			}
-			// LogPadrao.d("c.getContentLength():" + c.getContentLength());
-			long contentLength = c.getContentLength();
-	
-			File folder = new File(path);
-			folder.mkdirs();
-			File file = new File(folder, arquivo);
-			if (!file.exists()) {
-				file.createNewFile();
-			} else {
-				if ( file.length() == c.getContentLength())
-					return false;
-				throw new RuntimeException(UNABLE_TO_CREATE_EXISTING_FILE);
-			}
-			// LogPadrao.d("outputFile.exists():" + outputFile.exists());
-			// LogPadrao.d("outputFile.length():" + outputFile.length());
-			// LogPadrao.d("c.getContentLength():" + c.getContentLength());
-			InputStream is = c.getInputStream();;
-			c.connect();
-			FileOutputStream fos = new FileOutputStream(file);
-	
-			byte[] buffer = new byte[DEFAULT_BUFFER];
-			int len1 = 0;
-			int i = 0;
-			while ((len1 = is.read(buffer)) != -1) {
-				fos.write(buffer, 0, len1);
-				// LogPadrao.d("i++, (int) contentLength / 1024:" + (i++) +
-				// "/"
-				// +
-				// ((int) contentLength / 1024));
-				if (tarefa != null) {
-					if (contentLength > 0) {
-						tarefa.publicaProgresso(i++/(contentLength/1024));
-					} else {
-						tarefa.publicaProgresso(i++/(8000/1024));
-					}
-				}
-			}
-			fos.close();
-			is.close();
-			// TODO mover isto para cima
-			if (!GenericCommunication.isConnected())
-				GenericCommunication.connected = true;
-			return true;
-		} catch (IOException e){
-			throw new RuntimeException(e);
-		}
-	}
+//	public static boolean downloadFile(
+//			String urlString, String path, String arquivo,
+//			Tarefa<?, ?> tarefa, String sincronia
+//	) throws RuntimeException {
+//		try {
+//			// LogPadrao.d("Com:" + urlString);
+//			HttpURLConnection c;
+//			try {
+//				URL url = new URL(urlString);
+//				c = (HttpURLConnection) url.openConnection();
+//				c.setRequestMethod("GET");
+//				c.setDoOutput(true);
+//			} catch (MalformedURLException e) {
+//				LogPadrao.e(e);
+//				return false;
+//			} catch (ProtocolException e){
+//				LogPadrao.e (e);
+//				return false;
+//			}
+//			// LogPadrao.d("c.getContentLength():" + c.getContentLength());
+//			long contentLength = c.getContentLength();
+//	
+//			File folder = new File(path);
+//			folder.mkdirs();
+//			File file = new File(folder, arquivo);
+//			if (!file.exists()) {
+//				file.createNewFile();
+//			} else {
+//				if ( file.length() == c.getContentLength())
+//					return false;
+//				throw new RuntimeException(UNABLE_TO_CREATE_EXISTING_FILE);
+//			}
+//			// LogPadrao.d("outputFile.exists():" + outputFile.exists());
+//			// LogPadrao.d("outputFile.length():" + outputFile.length());
+//			// LogPadrao.d("c.getContentLength():" + c.getContentLength());
+//			InputStream is = c.getInputStream();;
+//			c.connect();
+//			FileOutputStream fos = new FileOutputStream(file);
+//	
+//			byte[] buffer = new byte[DEFAULT_BUFFER];
+//			int len1 = 0;
+//			int i = 0;
+//			while ((len1 = is.read(buffer)) != -1) {
+//				fos.write(buffer, 0, len1);
+//				// LogPadrao.d("i++, (int) contentLength / 1024:" + (i++) +
+//				// "/"
+//				// +
+//				// ((int) contentLength / 1024));
+//				if (tarefa != null) {
+//					if (contentLength > 0) {
+//						tarefa.publicaProgresso(i++/(contentLength/1024));
+//					} else {
+//						tarefa.publicaProgresso(i++/(8000/1024));
+//					}
+//				}
+//			}
+//			fos.close();
+//			is.close();
+//			// TODO mover isto para cima
+//			if (!GenericCommunication.isConnected())
+//				GenericCommunication.connected = true;
+//			return true;
+//		} catch (IOException e){
+//			throw new RuntimeException(e);
+//		}
+//	}
 
 
 }
