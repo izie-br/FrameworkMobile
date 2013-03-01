fd3d303155bcf60281846f026697bc9ae6614aa2
Igor Soares <igorsrs@github.com>
2013-03-01 16:22:58 +0000

    [BugFix]
    + Tentativa de correcao do "ConcurrentModificationException".

--- lib/src/main/java/com/quantium/mobile/framework/db/FirstLevelCache.java
+++ lib/src/main/java/com/quantium/mobile/framework/db/FirstLevelCache.java
@@ -8,23 +8,34 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class FirstLevelCache {
 
 	private final Map<EntityKey, Reference<?>> entityCache =
 			new HashMap<EntityKey, Reference<?>>();
-	int cachePushCount = 0;
 
-	public synchronized void pushToCache(Object klassId, Serializable keys [],
+	private int cachePushCount = 0;
+	private ReadWriteLock rwLock = new ReentrantReadWriteLock(false);
+
+	public void pushToCache(Object klassId, Serializable keys [],
 	                        Object entity)
 	{
-		// Conferir se eh proxy e extrair
-		EntityKey key = new EntityKey(klassId, keys);
-		entityCache.put(key, new SoftReference<Object>(entity));
-		// A cada 10 'pushes'
-		// conferir por itens nao utilizados
-		if ( (cachePushCount%10) == 0 ) {
-			trim();
+		Lock writeLock = rwLock.writeLock();
+		writeLock.lock();
+		try {
+			// Conferir se eh proxy e extrair
+			EntityKey key = new EntityKey(klassId, keys);
+			entityCache.put(key, new SoftReference<Object>(entity));
+			// A cada 10 'pushes'
+			// conferir por itens nao utilizados
+			if ( (cachePushCount%10) == 0 ) {
+				trim();
+			}
+		} finally {
+			writeLock.unlock();
 		}
 	}
 
@@ -36,13 +47,25 @@ public class FirstLevelCache {
 	}
 
 	public void removeFromCache(Object klassId, Serializable keys []) {
-		EntityKey key = new EntityKey(klassId, keys);
-		entityCache.remove(key);
+		Lock writeLock = rwLock.writeLock();
+		writeLock.lock();
+		try {
+			EntityKey key = new EntityKey(klassId, keys);
+			entityCache.remove(key);
+		} finally {
+			writeLock.unlock();
+		}
 	}
 
 	public Object cacheLookup(Object klassId, Serializable keys []){
-		EntityKey key = new EntityKey(klassId, keys);
-		return getOrRemoveIfNull(key);
+		Lock readLock = rwLock.readLock();
+		readLock.lock();
+		try {
+			EntityKey key = new EntityKey(klassId, keys);
+			return getOrRemoveIfNull(key);
+		} finally {
+			readLock.unlock();
+		}
 	}
 
 	private Object getOrRemoveIfNull(EntityKey key) {
@@ -60,15 +83,21 @@ public class FirstLevelCache {
 
 	@SuppressWarnings("unchecked")
 	public <T> Collection<Reference<T>> lookupForClass(Class<T> klass) {
-		ArrayList<Reference<T>> list =
-				new ArrayList<Reference<T>>();
-		for (EntityKey key : entityCache.keySet()) {
-			Object obj = getOrRemoveIfNull(key);
-			if ( obj != null && klass.isInstance(obj) ){
-				list.add(  new SoftReference<T>( (T)obj )  );
+		Lock readLock = rwLock.readLock();
+		readLock.lock();
+		try {
+			ArrayList<Reference<T>> list =
+					new ArrayList<Reference<T>>();
+			for (EntityKey key : entityCache.keySet()) {
+				Object obj = getOrRemoveIfNull(key);
+				if ( obj != null && klass.isInstance(obj) ){
+					list.add(  new SoftReference<T>( (T)obj )  );
+				}
 			}
+			return list;
+		} finally {
+			readLock.unlock();
 		}
-		return list;
 	}
 
 }
