78c1320a4c6895fa5ee13969f2dd5a492e9f080b
Igor Soares <igorsrs@github.com>
2013-03-06 14:43:49 +0000

    [Limpeza]
    + Movi a extracao de associacoes *-to-one para o ValueParser.

--- gerador/src/main/resources/DAO.java.d/mapToObject.java
+++ gerador/src/main/resources/DAO.java.d/mapToObject.java
@@ -10,6 +10,7 @@
             new CamelCaseUtils.AnyCamelMap<Object>();
         mapAnyCamelCase.putAll(map);
         Object temp;
+
 #foreach ($field in $fields)
 #**##if ($field.SerializationAlias)
 #******##set ($alias = $field.SerializationAlias)
@@ -17,34 +18,11 @@
 #******##set ($alias = $field.LowerCamel)
 #**##end
 #**###
-#**##if ($field.PrimaryKey)
-#******##set ($fallback = $defaultId)
-#**##elseif (${field.Klass} == "Long" || ${field.Klass} == "Double")
-#******##set ($fallback = "0")
-#**##elseif (${field.Klass} == "Boolean")
-#******##set ($fallback = "false")
-#**##else
-#******##set ($fallback = "null")
-#**##end
-#**###
 #**##if ($associationForField[$field])
 #******##set ($association = $associationForField[$field])
-#******##set ($submap = "submapFor${association.KeyToA}")
-#******#        Object ${submap} = mapAnyCamelCase.get("${association.KeyToA}");
-#******#        ${association.Klass} _${association.KeyToA} = null;
-#******#        if (${submap} != null && ${submap} instanceof Map){
-#******#            @SuppressWarnings("unchecked")
-#******#            Map<String,Object> submap = (Map<String,Object>)${submap};
-#******#            DAO<${association.Klass}> dao = this.factory.getDaoFor(${association.Klass}.class);
-#******#            _${association.KeyToA} = dao.mapToObject(submap);
-#******#        } else {
-#******#            temp = mapAnyCamelCase.get("${alias}");
-#******#            ${field.Type} _${field.LowerCamel} = parser.parse${field.Klass}(temp);
-#******#            if (_${field.LowerCamel} != ${defaultId}) {
-#******#                DAO<${association.Klass}> dao = this.factory.getDaoFor(${association.Klass}.class);
-#******#                _${association.KeyToA} = dao.get(_${field.LowerCamel});
-#******#            }
-#******#        }
+#******#        DAO<${association.Klass}> _${association.KeyToA}dao = this.factory.getDaoFor(${association.Klass}.class);
+#******#        ${association.Klass} _${association.KeyToA} = parser.extractAssociation(
+#******#                mapAnyCamelCase, _${association.KeyToA}dao, "${association.KeyToA}", "${alias}");
 #**##else
 #******#        temp = mapAnyCamelCase.get("${alias}");
 #******#        ${field.Type} _${field.LowerCamel} = parser.parse${field.Klass}(temp);
--- lib/src/main/java/com/quantium/mobile/framework/utils/ValueParser.java
+++ lib/src/main/java/com/quantium/mobile/framework/utils/ValueParser.java
@@ -1,68 +1,89 @@
 package com.quantium.mobile.framework.utils;
 
 import java.util.Date;
+import java.util.Map;
+
+import com.quantium.mobile.framework.DAO;
 
 public class ValueParser {
 
-    public long parseLong(Object value) {
-        if (value == null)
-            return 0;
-        if (value instanceof Number)
-            return ((Number)value).longValue();
-        if (value instanceof String) {
-            String strValue = (String) value;
-            return Long.parseLong(strValue);
-        }
-        throw new IllegalArgumentException();
-    }
+	public long parseLong(Object value) {
+		if (value == null)
+			return 0;
+		if (value instanceof Number)
+			return ((Number) value).longValue();
+		if (value instanceof String) {
+			String strValue = (String) value;
+			return Long.parseLong(strValue);
+		}
+		throw new IllegalArgumentException();
+	}
 
-    public double parseDouble(Object value) {
-        if (value == null)
-            return 0.0;
-        if (value instanceof Number)
-            return ((Number)value).doubleValue();
-        if (value instanceof String) {
-            String strValue = (String) value;
-            return Double.parseDouble(strValue);
-        }
-        throw new IllegalArgumentException(value.toString());
-    }
+	public double parseDouble(Object value) {
+		if (value == null)
+			return 0.0;
+		if (value instanceof Number)
+			return ((Number) value).doubleValue();
+		if (value instanceof String) {
+			String strValue = (String) value;
+			return Double.parseDouble(strValue);
+		}
+		throw new IllegalArgumentException(value.toString());
+	}
 
-    public boolean parseBoolean(Object value) {
-        if (value == null)
-            return false;
-        if (value instanceof Boolean)
-            return (Boolean)value;
-        if (value instanceof String) {
-            if (value.equals("0"))
-                return false;
-            else if (value.equals("1"))
-                return true;
-            else
-                return Boolean.parseBoolean((String)value);
-        }
-        if (value instanceof Number) {
-            long val = ((Number)value).longValue();
-            if (val == 0)
-               return false;
-            if (val == 1)
-               return true;
-        }
-        throw new IllegalArgumentException(value.toString());
-    }
+	public boolean parseBoolean(Object value) {
+		if (value == null)
+			return false;
+		if (value instanceof Boolean)
+			return (Boolean) value;
+		if (value instanceof String) {
+			if (value.equals("0"))
+				return false;
+			else if (value.equals("1"))
+				return true;
+			else
+				return Boolean.parseBoolean((String) value);
+		}
+		if (value instanceof Number) {
+			long val = ((Number) value).longValue();
+			if (val == 0)
+				return false;
+			if (val == 1)
+				return true;
+		}
+		throw new IllegalArgumentException(value.toString());
+	}
 
-    public Date parseDate(Object value) {
-        if (value == null)
-            return null;
-        if (value instanceof Date)
-            return (Date)value;
-        if (value instanceof String)
-            return DateUtil.stringToDate((String)value);
-        throw new IllegalArgumentException(value.toString());
-    }
+	public Date parseDate(Object value) {
+		if (value == null)
+			return null;
+		if (value instanceof Date)
+			return (Date) value;
+		if (value instanceof String)
+			return DateUtil.stringToDate((String) value);
+		throw new IllegalArgumentException(value.toString());
+	}
 
-    public String parseString(Object value) {
-        return (value == null)? null : value.toString();
-    }
+	public String parseString(Object value) {
+		return (value == null) ? null : value.toString();
+	}
+
+	public <T> T extractAssociation(
+			Map<?, ?> allFieldsMap, DAO<T> associationDao,
+			String submapKey, String foreignKey)
+	{
+		Object temp = allFieldsMap.get(submapKey);
+		if (temp != null && temp instanceof Map) {
+			@SuppressWarnings("unchecked")
+			Map<String, Object> submap = (Map<String, Object>) temp;
+			return associationDao.mapToObject(submap);
+		} else {
+			temp = allFieldsMap.get(foreignKey);
+			long associationId = parseLong(temp);
+			return (associationId == 0)?
+					null :
+					associationDao.get(associationId);
+		}
+	}
 
 }
