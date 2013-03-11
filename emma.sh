80317bf2d053b47cbeaf17f1ecce2120c7d514e1
Igor Soares <igorsrs@github.com>
2013-03-11 19:06:28 +0000

    [Testes]
    * Adicionei um script bahs para criar o report do emma na pasta "coverage".

---
+++ emma.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+java -cp ~/.m2/repository/emma/emma/2.1.5320/emma-2.1.5320.jar emma report -r html -in lib_jdbc/target/coverage.em,lib_android/target/coverage.em,test/target/coverage.em,lib_jdbc/coverage.ec,lib_android/coverage.ec,test/target/emma/coverage.ec -sp lib_android/src/main/java,lib_jdbc/src/main/java,test/src/main/java
