90831bf62a80144b6ca0a624aa89174d993880b4
Igor Soares <igorsrs@github.com>
2013-03-18 11:33:33 +0000

    Comentarios com exemplo de trimLimiter Timer.

--- gerador/src/main/resources/SQLiteDAOFactory.java
+++ gerador/src/main/resources/SQLiteDAOFactory.java
@@ -9,6 +9,23 @@ import com.quantium.mobile.framework.utils.ValueParser;
 public abstract class SQLiteDAOFactory extends FirstLevelCache
         implements DAOFactory
 {
+// Exemplo de TrimTimer
+//
+//    private Timer trimTimer = new Timer(/*isDaemon=*/true);
+//    {
+//        trimTimer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                trim();
+//            }
+//        }, 15*1000, 15*1000);
+//    }
+//
+//    @Override
+//    protected void finalize() throws Throwable {
+//        this.trimTimer.cancel();
+//        super.finalize();
+//    }
 
     private ValueParser parser;
 
