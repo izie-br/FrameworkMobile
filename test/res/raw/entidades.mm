5e5334ca280f21a641d6efbe64f84179acaf8648
Marcelo Marra <marcelo.marra@gmail.com>
2013-07-15 20:51:57 +0000

    Movendo pluralizacao

--- test/res/raw/entidades.mm
+++ test/res/raw/entidades.mm
@@ -1,26 +1,12 @@
 
 <map version="0.9.0">
-    <node TEXT="Mind Map">
-        <node ID="E8014917-AC18-4326-A084-650A35306939" TEXT="Two Words:Two Word" FOLDED="false" COLOR="#000000">
+    <node ID="6CEFA339-DA8D-4FC2-B60E-35024923AF56" TEXT="Mind Map" FOLDED="false">
+        <font NAME="LucidaGrande" SIZE="12"></font>
+        <edge COLOR="#7F7F7F"></edge>
+        <node ID="348050E1-7E40-4A5C-982B-EEDF649B2ADD" TEXT="Authors:Author" POSITION="right" FOLDED="false" COLOR="#000000">
             <font NAME="LucidaGrande" SIZE="13"></font>
             <edge COLOR="#7F7F7F"></edge>
-            <node ID="0A714984-3415-4C02-BCF0-3CC5ED35C1B6" TEXT="Module:Document" POSITION="right" FOLDED="false" COLOR="#000000">
-                <font NAME="LucidaGrande" SIZE="12"></font>
-                <edge COLOR="#FFCC66"></edge>
-            </node>
-            <node ID="67D936BF-E090-41BE-8588-B680AE5E36E2" TEXT="Name" POSITION="right" FOLDED="false" COLOR="#000000">
-                <font NAME="LucidaGrande" SIZE="12"></font>
-                <edge COLOR="#CCFF66"></edge>
-                <node ID="48CB775D-572F-4678-B43D-FEC6DD6432C3" TEXT="type:String" FOLDED="false" COLOR="#000000">
-                    <font NAME="LucidaGrande" SIZE="12"></font>
-                    <edge COLOR="#CCFF66"></edge>
-                </node>
-            </node>
-        </node>
-        <node ID="348050E1-7E40-4A5C-982B-EEDF649B2ADD" TEXT="Authors:Author" FOLDED="false" COLOR="#000000">
-            <font NAME="LucidaGrande" SIZE="13"></font>
-            <edge COLOR="#7F7F7F"></edge>
-            <node ID="76809DD6-F870-4957-9458-1B483A5D353D" TEXT="Active" POSITION="left" FOLDED="false" COLOR="#000000">
+            <node ID="76809DD6-F870-4957-9458-1B483A5D353D" TEXT="Active" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#66CCFF"></edge>
                 <node ID="B9745ED8-E510-4377-AE03-B53FB37EE45D" TEXT="type:Boolean" FOLDED="false" COLOR="#000000">
@@ -28,7 +14,7 @@
                     <edge COLOR="#66CCFF"></edge>
                 </node>
             </node>
-            <node ID="CD8CC728-3D07-4BFE-89A7-71B5280B7DC9" TEXT="Name" POSITION="left" FOLDED="false" COLOR="#000000">
+            <node ID="CD8CC728-3D07-4BFE-89A7-71B5280B7DC9" TEXT="Name" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#66FFCC"></edge>
                 <node ID="E0C76EAA-C38E-491B-A4D1-8C2F82A7FE07" TEXT="type:String" FOLDED="false" COLOR="#000000">
@@ -52,7 +38,7 @@
                     <edge COLOR="#66FFCC"></edge>
                 </node>
             </node>
-            <node ID="F975A0C2-FBA6-4DFE-B477-C4DAABB15608" TEXT="Created At" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="F975A0C2-FBA6-4DFE-B477-C4DAABB15608" TEXT="Created At" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#CCFF66"></edge>
                 <node ID="D53DA5D8-EE6E-4547-83AD-866BB5C640CD" TEXT="type:Date" FOLDED="false" COLOR="#000000">
@@ -65,14 +51,14 @@
                 </node>
             </node>
         </node>
-        <node ID="3A3D915B-E64E-4353-84D9-ECBD173B2AB2" TEXT="Documents:Document" FOLDED="false" COLOR="#000000">
+        <node ID="3A3D915B-E64E-4353-84D9-ECBD173B2AB2" TEXT="Documents:Document" POSITION="left" FOLDED="false" COLOR="#000000">
             <font NAME="LucidaGrande" SIZE="13"></font>
             <edge COLOR="#7F7F7F"></edge>
-            <node ID="98CEFB7D-5113-4F38-B92A-6DBD49187F12" TEXT="Module:Document" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="98CEFB7D-5113-4F38-B92A-6DBD49187F12" TEXT="Module:Document" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#CC66FF"></edge>
             </node>
-            <node ID="A14C8549-F68D-4A89-AF66-BE904E4DB8EE" TEXT="Title" POSITION="left" FOLDED="false" COLOR="#000000">
+            <node ID="A14C8549-F68D-4A89-AF66-BE904E4DB8EE" TEXT="Title" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#66CCFF"></edge>
                 <node ID="6D82F7D9-E2F8-477C-8DBF-6233F0080AA0" TEXT="type:String" FOLDED="false" COLOR="#000000">
@@ -84,7 +70,7 @@
                     <edge COLOR="#66CCFF"></edge>
                 </node>
             </node>
-            <node ID="FE789F86-50E6-4547-9564-CCEFA956A487" TEXT="Created At" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="FE789F86-50E6-4547-9564-CCEFA956A487" TEXT="Created At" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#FFCC66"></edge>
                 <node ID="BEAE8336-DA8B-4965-BA68-CBB1E43E928D" TEXT="type:Date" FOLDED="false" COLOR="#000000">
@@ -92,7 +78,7 @@
                     <edge COLOR="#FFCC66"></edge>
                 </node>
             </node>
-            <node ID="BE65AB6E-7954-41D5-B913-9FB97790E498" TEXT="Text" POSITION="left" FOLDED="false" COLOR="#000000">
+            <node ID="BE65AB6E-7954-41D5-B913-9FB97790E498" TEXT="Text" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#66FFCC"></edge>
                 <node ID="144AE0C5-2CD2-4C14-9D25-C985251C24B8" TEXT="type:String" FOLDED="false" COLOR="#000000">
@@ -104,7 +90,7 @@
                     <edge COLOR="#66FFCC"></edge>
                 </node>
             </node>
-            <node ID="B4D2C776-6D2F-409A-A9FF-BD7D0AC37E3C" TEXT="Author" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="B4D2C776-6D2F-409A-A9FF-BD7D0AC37E3C" TEXT="Author" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#CCFF66"></edge>
                 <node ID="FDA8D677-712C-48D9-B812-FDAC352BCDF1" TEXT="type:Object of Authors" FOLDED="false" COLOR="#000000">
@@ -113,10 +99,10 @@
                 </node>
             </node>
         </node>
-        <node ID="E2D91705-E002-4235-B5DD-00DA6BCB3C9D" TEXT="Scores:Score" FOLDED="false" COLOR="#000000">
+        <node ID="E2D91705-E002-4235-B5DD-00DA6BCB3C9D" TEXT="Scores:Score" POSITION="right" FOLDED="false" COLOR="#000000">
             <font NAME="LucidaGrande" SIZE="13"></font>
             <edge COLOR="#7F7F7F"></edge>
-            <node ID="B1E7A72C-45C4-45A6-BEE1-20317A3520A8" TEXT="Score" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="B1E7A72C-45C4-45A6-BEE1-20317A3520A8" TEXT="Score" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#FFCC66"></edge>
                 <node ID="E01643B7-CF9D-49D4-BCCE-D2390E1FAB5A" TEXT="type:Integer" FOLDED="false" COLOR="#000000">
@@ -132,7 +118,7 @@
                     <edge COLOR="#FFCC66"></edge>
                 </node>
             </node>
-            <node ID="0D401034-30CC-4078-9053-05F1D0C458EA" TEXT="Document" POSITION="left" FOLDED="false" COLOR="#000000">
+            <node ID="0D401034-30CC-4078-9053-05F1D0C458EA" TEXT="Document" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#66FFCC"></edge>
                 <node ID="477BB265-7ED5-4533-BF54-87BA844EB666" TEXT="type:Object of Documents" FOLDED="false" COLOR="#000000">
@@ -144,7 +130,7 @@
                     <edge COLOR="#66FFCC"></edge>
                 </node>
             </node>
-            <node ID="23E1F761-38A7-451A-B08F-9B25DE6C5A37" TEXT="Author" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="23E1F761-38A7-451A-B08F-9B25DE6C5A37" TEXT="Author" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#CCFF66"></edge>
                 <node ID="5CDB37F5-5D5A-46F7-B96C-822B07A92FB6" TEXT="type:Object of Authors" FOLDED="false" COLOR="#000000">
@@ -157,10 +143,10 @@
                 </node>
             </node>
         </node>
-        <node ID="C84E5AD2-918E-4766-A31A-754094300370" TEXT="Customers:Customer" FOLDED="false" COLOR="#000000">
+        <node ID="C84E5AD2-918E-4766-A31A-754094300370" TEXT="Customers:Customer" POSITION="left" FOLDED="false" COLOR="#000000">
             <font NAME="LucidaGrande" SIZE="13"></font>
             <edge COLOR="#7F7F7F"></edge>
-            <node ID="A8ACEBD3-718F-4A2C-B959-D64400E127CD" TEXT="Name" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="A8ACEBD3-718F-4A2C-B959-D64400E127CD" TEXT="Name" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#FFCC66"></edge>
                 <node ID="C2BACCCE-F719-4430-B2F5-1335E65A1F31" TEXT="type:String" FOLDED="false" COLOR="#000000">
@@ -172,7 +158,7 @@
                     <edge COLOR="#FFCC66"></edge>
                 </node>
             </node>
-            <node ID="EE3F5032-CE03-4477-916F-1F9C6921008D" TEXT="Documents" POSITION="right" FOLDED="false" COLOR="#000000">
+            <node ID="EE3F5032-CE03-4477-916F-1F9C6921008D" TEXT="Documents" FOLDED="false" COLOR="#000000">
                 <font NAME="LucidaGrande" SIZE="12"></font>
                 <edge COLOR="#CCFF66"></edge>
                 <node ID="058114F6-13FA-4A0D-8453-4C2A085DCF97" TEXT="type:N-ary List of Documents" FOLDED="false">
