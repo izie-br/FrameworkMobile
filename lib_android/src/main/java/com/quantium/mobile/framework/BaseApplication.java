d078e972a96fb5713da873fd9e88fe1e30b373c2
Igor Soares <jenkins@izie.com.br>
2014-10-24 22:48:46 +0000

    Correções de bugs.

--- lib_android/src/main/java/com/quantium/mobile/framework/BaseApplication.java
+++ lib_android/src/main/java/com/quantium/mobile/framework/BaseApplication.java
@@ -4,26 +4,25 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
-import com.quantium.mobile.framework.logging.LogPadrao;
 
-public abstract class BaseApplication extends MultiDexApplication {
+public abstract class BaseApplication extends MultiDexApplication{
 
-    protected static BaseApplication instance;
+	protected static BaseApplication instance;
     private boolean installed = false;
 
-    public abstract int dbVersion();
+	public abstract int dbVersion();
 
-    public abstract String userName();
+	public abstract String userName();
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        BaseApplication.instance = this;
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		BaseApplication.instance = this;
+	}
 
-    public static Context getContext() {
-        return BaseApplication.instance.getApplicationContext();
-    }
+	public static Context getContext(){
+		return BaseApplication.instance.getApplicationContext();
+	}
 
 //	public static SharedPreferences getPreferences(){
 //		return getContext().getSharedPreferences(getContext().getPackageName(), Context.MODE_PRIVATE);
@@ -33,13 +32,13 @@ public abstract class BaseApplication extends MultiDexApplication {
 //		return getContext().getSharedPreferences(getContext().getPackageName()+nome, Context.MODE_PRIVATE);
 //	}
 
-    public static int getDBVersion() {
-        return instance.dbVersion();
-    }
+	public static int getDBVersion(){
+		return instance.dbVersion();
+	}
 
-    public static String getUserName() {
-        return instance.userName();
-    }
+	public static String getUserName(){
+		return instance.userName();
+	}
 
     public boolean isInstalled() {
         return installed;
@@ -47,12 +46,9 @@ public abstract class BaseApplication extends MultiDexApplication {
 
     @Override
     protected final void attachBaseContext(Context base) {
-        try {
-            super.attachBaseContext(base);
-            installed = true;
-        } catch (Throwable t) {
-            LogPadrao.e(t);
-        }
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+        installed = true;
     }
 
 }
